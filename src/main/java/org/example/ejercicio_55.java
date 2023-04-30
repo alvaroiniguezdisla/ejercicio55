26401f00fb1b802605f8bb82573c5231da7fa704
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-30 17:27:24 +0200

    Ejercicio 55 terminado

--- src/main/java/org/example/ejercicio_55.java
+++ src/main/java/org/example/ejercicio_55.java
@@ -1,2 +1,23 @@
-package org.example;public class ejercicio_55 {
+package org.example;
+
+public class ejercicio_55 {
+    //creamos el metodo factorial
+    public static int factorial(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+
+    public static int taylor(int x){
+        int sum=0;
+        for(int i=0;i<10;i++){
+            sum+=Math.pow(x,i)/factorial(i);
+
+        }
+        return sum;
+    }
+
 }
+
--- src/test/java/org/example/Test_ej55.java
+++ src/test/java/org/example/Test_ej55.java
@@ -1,2 +1,20 @@
-package org.example;public class Test_ej55 {
+package org.example;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class Test_ej55 {
+
+    //creame varios tests del metodo de taylor para los casos extremos
+    @Test
+    public void test_taylor() {
+        assertEquals(1, ejercicio_55.taylor(0));
+        assertEquals(2, ejercicio_55.taylor(1));
+        assertEquals(3, ejercicio_55.taylor(2));
+        assertEquals(4, ejercicio_55.taylor(3));
+
+    }
+
+
+
 }
